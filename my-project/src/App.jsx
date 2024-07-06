cd5b6618b7a9f3825d68598c6c75a2f0ee61fc94
sabbir ahmed <sabbirahmed.info33@gmail.com>
2024-07-06 17:01:36 +0600

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,29 @@
+
+
+function App() {
+
+
+  return (
+    <>
+  <section className=" w-1/2 mx-auto">
+  <div className="hero bg-base-200 min-h-screen ">
+  <div className="hero-content flex-col lg:flex-row">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+      className="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+  </section>
+    </>
+  )
+}
+
+export default App
